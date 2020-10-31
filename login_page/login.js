b5cfbc6b8c2ca8c13ceb99fc9b2d4f3392a7ab5d
aissa <ouboukioudaissa@gmail.com>
2020-10-31 17:57:48 +0100

    the home page and the login page of the est website

---
+++ login_page/login.js
@@ -0,0 +1,8 @@
+const login = document.getElementById('login');
+
+login.addEventListener('submit', (e) => {
+	console.log('the form submited');
+	// this is where you can do some ajax
+	// request to a backend to authenticate the user
+	// to thier profile
+})
